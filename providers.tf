107fa472747bc2fecb97319410e9c0c7992c8e68
Joel William <joel.william@celebaltech.com>
2026-09-02 15:58:10 +0530

    remove access key

--- providers.tf
+++ providers.tf
@@ -8,7 +8,6 @@ terraform {
   backend "azurerm" {
     storage_account_name = "tfremotebackendstgacc"
     container_name       = "tf-remote-backend-container"
-    access_key           = "eFJy/hoXfZid0SXQLHLRwkTtGiU0dZT3ssGJQ3NAlltcqvTCDO+BtwwC5k31j9rzA9OmKmi4bQYl+AStJWzQOQ=="
   }
 }
 
